71fcbb05043239d26e8c8760afc04097d4463b25
Lucho <luis.munoz.caceres@ug.uchile.cl>
2025-10-08 18:18:41 -0300

    me voy a matar

--- Tarea_1/bin/experimentation.cpp
+++ Tarea_1/bin/experimentation.cpp
@@ -4,33 +4,32 @@
 
 int main(){
     // Transformamos la lista en los N correspondientes
-    /*for (int i = 0; i<1; i++){// testeamos para N 2 ** 15, despues lo vamos alargando (i<=11)
+    const std::string filename = "../tree.bin";
+    std::ifstream in("../datos.bin", std::ios::binary);
+    std::pair<int,float> par;
+
+    for (int i = 0; i<11; i++){// testeamos para N 2 ** 15, despues lo vamos alargando (i<=11)
+        BTree BTree(filename);
+        BTreeNode node = TreeUtils::crear_raiz();
         int N = pow(2, 15 + i);
+
         for (int j = 0; j <= N; j++){
-            printf("%d\n", j);
+            std::streampos file_offset = i * sizeof(std::pair<int,float>);
+            in.seekg(file_offset);
+            in.read(reinterpret_cast<char *>(&par), sizeof(std::pair<int,float>));
+            std::cout << "Iteracion N=2^"<< 15+i <<" se inserta el par "<< par.second << " Iteraciones pendientes" << N-j << std::endl;
+            BTree.insert(par, node, 0);
         };
-    }*/
-   const std::string filename = "../tree.bin";
 
-   BTree BTree(filename);
-   BTreeNode node = TreeUtils::crear_raiz();
+        std::cout << "Escrituras para N=2^"<< 15+1 << BTree.escrituras << std::endl;
+        std::cout << "Lecturas para N=2^"<< 15+1 << BTree.lecturas << std::endl;
+        std::cout << "Nodos para N=2^"<< 15+1 << BTree.nodos.size() << std::endl;
+        
+    }
 
-   std::ifstream in("../datos.bin", std::ios::binary);
-   std::pair<int,float> par;
-
-   for (int i = 0; i<342; i++){
-        std::streampos file_offset = i * sizeof(std::pair<int,float>);
-        in.seekg(file_offset);
-        in.read(reinterpret_cast<char *>(&par), sizeof(std::pair<int,float>));
-        std::cout << "se agregara; "<< par.second << std::endl;
-        BTree.insert(par, node, 0);
-   }
 
    //TreeUtils::write_node(filename, node, 0);
-
-   std::string fileout = "../tree.bin";
-   BTreeNode node2 = TreeUtils::readNode(fileout, 1);
-   std::cout << "olaaaaa" << node2.llaves_valores[0].second << std::endl;
+   
 
    return 0;
 };
